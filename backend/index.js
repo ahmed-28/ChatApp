8cba8840d1b181226f4a080794b07e2dc1ac115c
ahmed-28 <ahmedshamsudeen2000@gmail.com>
2020-06-11 15:00:00 +0100

    create chat component

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ io.on('connection', (socket) => {
     socket.on('sendMessage' , (message,callback) => {
         const user = getUser(socket.id);
         io.to(user.room).emit('message',{user:user.name,text:message});
-        callback();
+        callback(); 
     });
     socket.on('disconnect' , () => {
         console.log("user connection ended");
