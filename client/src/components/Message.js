3f8bb37fd3beaac58b08a539e69ccad878957391
ahmed-28 <ahmedshamsudeen2000@gmail.com>
2020-06-24 11:00:00 +0100

    Remove users on discoonect

--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -9,7 +9,7 @@ const Message = ({message,name}) => {
     return(
         curUser ? (
             <div className="absolute inset-y-0 right-0 border-2 border-red-500">
-                hey
+                u are user
             </div>
         ) : (
             <div className="absolute inset-y-0 left-0 border-2 border-red-500">
